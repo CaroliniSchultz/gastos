dd95868d45d280ee4b79dec6441f3f0e65ea6b85
CaroliniSchultz <149988999+CaroliniSchultz@users.noreply.github.com>
2026-06-30 21:27:11 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,10 +6,11 @@
 // ============================================================
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "gastos-xxxxx.firebaseapp.com",
-  projectId: "gastos-xxxxx",
-  storageBucket: "gastos-xxxxx.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcxyz123"
+  apiKey: "AIzaSyDAFNUq6PLi8OhBybFQGAtgHLTxhn8Vs04",
+  authDomain: "gastos-3bcb2.firebaseapp.com",
+  projectId: "gastos-3bcb2",
+  storageBucket: "gastos-3bcb2.firebasestorage.app",
+  messagingSenderId: "454435610652",
+  appId: "1:454435610652:web:738a644febe0073a6bffd9",
+  measurementId: "G-141W40ZN45"
 };
